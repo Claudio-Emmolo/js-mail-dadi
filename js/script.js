13b02f94ecf02275ea832502fea1ef0ec8268e22
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-24 15:13:22 +0100

    Print result on html

--- js/script.js
+++ js/script.js
@@ -10,17 +10,16 @@ startGame.addEventListener ("click", function(){
     numberPlayerElement = Math.floor(Math.random() * 6) + 1;
     numberComputerElement = Math.floor(Math.random() * 6) + 1;
 
-    console.log(numberPlayerElement);
-    console.log(numberComputerElement);
+    document.getElementById("user-number") .innerHTML = `Hai tirato: ${numberPlayerElement}`;
+    document.getElementById("computer-number") .innerHTML = `Il computer ha tirato: ${numberComputerElement}`;
 
     if (numberPlayerElement > numberComputerElement){
-        console.log ("Hai vinto!");
+        document.getElementById("result") .innerHTML = "Hai vinto!";
     } else if ((numberPlayerElement < numberComputerElement)){
-        console.log ("Hai perso");
+        document.getElementById("result") .innerHTML = "Hai perso!";
     } else if (numberPlayerElement == numberComputerElement){
-        console.log("Pareggio")
+        document.getElementById("result") .innerHTML = "Pareggio!";
     }
-
 });
     
 
