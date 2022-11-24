f6467654ea0e6b9d895d5ea898fae35f869aec4b
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-24 15:18:09 +0100

    Create Array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 //Definisco gli Array
-
+const emailList = [
+    "giggi0209@gmail.com",
+    "riccardo.pet@gmail.com",
+    "luigi.mic@gmail.com",
+    "stefano.cap@gmail.com",
+    "claudioemmolo90@gmail.com",
+    "gold-email.1998@gmail.com",
+    "giggi0209@gmail.com",
+    "pippi@gmail.com",
+    "cane@gmail.com",
+    "gatto@gmail.com",
+    "carroarmato-96@gmail.com",
+    "jshelp@info.com",
+    "htmlcsshrlp@info.com",
+    "boolean81@class.com",
+];
 
 //Definisco le variabili
 const startGame = document.getElementById("btn-start");
@@ -24,5 +39,7 @@ startGame.addEventListener ("click", function(){
         document.getElementById("result") .innerHTML = "Pareggio!";
     }
 });
-    
+
+
+//Email
 
