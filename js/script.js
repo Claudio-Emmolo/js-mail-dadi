14c17aebf6146cfbdd7cb64e3f0f31b1cf5367b5
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-24 14:52:18 +0100

    Create random number 1 to 6

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const startGame = document.getElementById("btn-start");
+
+//Definisco gli Array
+let numberPlayerElement = Math.floor(Math.random() * 6) + 1;
+
+let numberComputerElement = Math.floor(Math.random() * 6) + 1;
+
+console.log(numberPlayerElement);
+console.log(numberComputerElement);
+    
+
